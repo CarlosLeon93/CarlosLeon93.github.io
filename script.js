daf7e9dcbf17773e83b88d3e9d71220ac4403bdc
Carlos <cals.1993@protonmail.com>
2020-03-16 23:25:19 +0100

    Version 12

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //alert("La resolucion de la pantalla es" + screen.width + "x" + screen.height)
-
 function myFunction() {
-    window.alert("Info telephone number: +46736914525")
+    window.alert("More information: +46736914525")
+}
+
+$(document).ready(main);
+
+var contador = true;
+
+function main() {
+    $('.bars').click(function() {
+        //$('nav').toggle();
+        if (contador == true) {
+            $('nav').animate({
+                left: '0%'
+            });
+            contador = false;
+
+        } else {
+            $('nav').animate({
+                left: '-100%'
+            });
+            contador = true;
+
+        }
+
+    });
 }
