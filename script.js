665125494e5baaf45e61bcc08a5e7fbd318555fc
Carlos <cals.1993@protonmail.com>
2020-03-19 02:17:15 +0100

    Version 13

--- script.js
+++ script.js
@@ -6,22 +6,35 @@ function myFunction() {
 $(document).ready(main);
 
 var contador = true;
+var userHasScrolled = false;
 
 function main() {
+
     $('.bars').click(function() {
         //$('nav').toggle();
+        window.onscroll = function() {
+            userHasScrolled = true;
+        }
         if (contador == true) {
+
             $('nav').animate({
                 left: '0%'
             });
             contador = false;
 
+
+        } else if (contador == false && userHasScrolled == true) {
+
+            $('nav').animate({
+                left: '-100%'
+            });
+            contador = true;
+            userHasScrolled = false;
         } else {
             $('nav').animate({
                 left: '-100%'
             });
             contador = true;
-
         }
 
     });
